5805c763e119575aa13145e8774b49825e116e5b
MatteoMit <matteo.mitrano@3mlinformatica.it>
2019-11-29 08:49:45 +0100

    1 file modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { ParentComponentComponent } from './parent-component/parent-component.component';
+import { ParentComponent } from './parent.component';
+import { ChildComponent } from './child.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ParentComponentComponent ],
+  declarations: [ AppComponent, HelloComponent, ParentComponent, ChildComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
