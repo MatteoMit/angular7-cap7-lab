fde0708d0d5a525434e21717e19a562362cbc8f0
MatteoMit <matteo.mitrano@3mlinformatica.it>
2019-11-29 09:23:44 +0100

    2 files modified

--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -1,12 +1,19 @@
 import { Component } from "@angular/core";
-
 @Component({
   selector: "app-child",
   template: `
-    <div>
+    <div class="heading">
+      <ng-content select="h1"></ng-content>
+    </div>
+    <div class="main">
+      <ng-content select=".text"></ng-content>
+    </div>
+    <div class="component">
+      <ng-content select="app-random"></ng-content>
+    </div>
+    <div class="action">
       <ng-content></ng-content>
     </div>
-  `,
-  styles: []
+  `
 })
 export class ChildComponent {}
--- src/app/parent.component.ts
+++ src/app/parent.component.ts
@@ -3,7 +3,13 @@ import { Component } from "@angular/core";
   selector: "app-root",
   template: `
     <app-child>
+      <button>Condividi</button>
+      <span>Span element</span>
+
       <h1>{{ title }}</h1>
+      <p class="text">
+        Maecenas faucibus mollis interdum.
+      </p>
       <app-random></app-random>
     </app-child>
   `
